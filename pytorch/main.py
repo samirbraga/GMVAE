9db88fa1bed660e5444b3337c954953c9b06de96
samirbraga <samirchaves@gmail.com>
2022-01-27 21:01:35 -0300

    Implementing save and load methods in GMVAE model

--- pytorch/main.py
+++ pytorch/main.py
@@ -7,7 +7,6 @@ Main file to execute the model on the MNIST dataset
 
 """
 import matplotlib
-matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import argparse
 import random
@@ -18,6 +17,8 @@ from torchvision import datasets, transforms
 from torch.utils.data.sampler import SubsetRandomSampler
 import torch.utils.data
 from model.GMVAE import *
+
+matplotlib.use('agg')
 
 #########################################################
 ## Input Parameters
@@ -48,7 +49,7 @@ parser.add_argument('--batch_size_val', default=200, type=int,
                     help='mini-batch size of validation (default: 200)')
 parser.add_argument('--learning_rate', default=1e-3, type=float,
                     help='learning rate (default: 0.001)')
-parser.add_argument('--decay_epoch', default=-1, type=int, 
+parser.add_argument('--decay_epoch', default=-1, type=int,
                     help='Reduces the learning rate every decay_epoch')
 parser.add_argument('--lr_decay', default=0.5, type=float,
                     help='Learning rate decay for training (default: 0.5)')
@@ -68,12 +69,12 @@ parser.add_argument('--train_proportion', default=1.0, type=float,
 ## Gumbel parameters
 parser.add_argument('--init_temp', default=1.0, type=float,
                     help='Initial temperature used in gumbel-softmax (recommended 0.5-1.0, default:1.0)')
-parser.add_argument('--decay_temp', default=1, type=int, 
+parser.add_argument('--decay_temp', default=1, type=int,
                     help='Set 1 to decay gumbel temperature at every epoch (default: 1)')
-parser.add_argument('--hard_gumbel', default=0, type=int, 
+parser.add_argument('--hard_gumbel', default=0, type=int,
                     help='Set 1 to use the hard version of gumbel-softmax (default: 1)')
-parser.add_argument('--min_temp', default=0.5, type=float, 
-                    help='Minimum temperature of gumbel-softmax after annealing (default: 0.5)' )
+parser.add_argument('--min_temp', default=0.5, type=float,
+                    help='Minimum temperature of gumbel-softmax after annealing (default: 0.5)')
 parser.add_argument('--decay_temp_rate', default=0.013862944, type=float,
                     help='Temperature decay rate at every epoch (default: 0.013862944)')
 
@@ -96,7 +97,7 @@ parser.add_argument('--random_search_it', type=int, default=20,
 args = parser.parse_args()
 
 if args.cuda == 1:
-   os.environ["CUDA_VISIBLE_DEVICES"] = str(args.gpuID)
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(args.gpuID)
 
 ## Random Seed
 SEED = args.seed
@@ -104,37 +105,40 @@ np.random.seed(SEED)
 random.seed(SEED)
 torch.manual_seed(SEED)
 if args.cuda:
-  torch.cuda.manual_seed(SEED)
+    torch.cuda.manual_seed(SEED)
 
 #########################################################
 ## Read Data
 #########################################################
 if args.dataset == "mnist":
-  print("Loading mnist dataset...")
-  # Download or load downloaded MNIST dataset
-  train_dataset = datasets.MNIST('./mnist', train=True, download=True, transform=transforms.ToTensor())
-  test_dataset = datasets.MNIST('./mnist', train=False, transform=transforms.ToTensor())
+    print("Loading mnist dataset...")
+    # Download or load downloaded MNIST dataset
+    train_dataset = datasets.MNIST('./mnist', train=True, download=True, transform=transforms.ToTensor())
+    test_dataset = datasets.MNIST('./mnist', train=False, transform=transforms.ToTensor())
 
 
 #########################################################
 ## Data Partition
 #########################################################
 def partition_dataset(n, proportion=0.8):
-  train_num = int(n * proportion)
-  indices = np.random.permutation(n)
-  train_indices, val_indices = indices[:train_num], indices[train_num:]
-  return train_indices, val_indices
+    train_num = int(n * proportion)
+    indices = np.random.permutation(n)
+    train_indices, val_indices = indices[:train_num], indices[train_num:]
+    return train_indices, val_indices
+
 
 if args.train_proportion == 1.0:
-  train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
-  test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size_val, shuffle=False)
-  val_loader = test_loader
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size_val, shuffle=False)
+    val_loader = test_loader
 else:
-  train_indices, val_indices = partition_dataset(len(train_dataset), args.train_proportion)
-  # Create data loaders for train, validation and test datasets
-  train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, sampler=SubsetRandomSampler(train_indices))
-  val_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size_val, sampler=SubsetRandomSampler(val_indices))
-  test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size_val, shuffle=False)
+    train_indices, val_indices = partition_dataset(len(train_dataset), args.train_proportion)
+    # Create data loaders for train, validation and test datasets
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size,
+                                               sampler=SubsetRandomSampler(train_indices))
+    val_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size_val,
+                                             sampler=SubsetRandomSampler(val_indices))
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size_val, shuffle=False)
 
 ## Calculate flatten size of each input data
 args.input_size = np.prod(train_dataset[0][0].size())
@@ -150,5 +154,4 @@ history_loss = gmvae.train(train_loader, val_loader)
 ## Testing Phase
 accuracy, nmi = gmvae.test(test_loader)
 print("Testing phase...")
-print("Accuracy: %.5lf, NMI: %.5lf" % (accuracy, nmi) )
-
+print("Accuracy: %.5lf, NMI: %.5lf" % (accuracy, nmi))
--- pytorch/networks/Networks.py
+++ pytorch/networks/Networks.py
@@ -6,132 +6,160 @@
 Gaussian Mixture Variational Autoencoder Networks
 
 """
+import os
 import torch
 import torch.nn.init as init
 from torch import nn
 from torch.nn import functional as F
 from networks.Layers import *
 
+
+class StorableModel(nn.Module):
+    def save(self, path):
+        torch.save(self.state_dict(), path)
+
+    def load(self, path):
+        self.load_state_dict(torch.load(path))
+
+
 # Inference Network
-class InferenceNet(nn.Module):
-  def __init__(self, x_dim, z_dim, y_dim):
-    super(InferenceNet, self).__init__()
+class InferenceNet(StorableModel):
+    def __init__(self, x_dim, z_dim, y_dim):
+        super(InferenceNet, self).__init__()
+
+        # q(y|x)
+        self.inference_qyx = torch.nn.ModuleList([
+            nn.Linear(x_dim, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            GumbelSoftmax(512, y_dim)
+        ])
+
+        # q(z|y,x)
+        self.inference_qzyx = torch.nn.ModuleList([
+            nn.Linear(x_dim + y_dim, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            Gaussian(512, z_dim)
+        ])
 
     # q(y|x)
-    self.inference_qyx = torch.nn.ModuleList([
-        nn.Linear(x_dim, 512),
-        nn.ReLU(),
-        nn.Linear(512, 512),
-        nn.ReLU(),
-        GumbelSoftmax(512, y_dim)
-    ])
+    def qyx(self, x, temperature, hard):
+        num_layers = len(self.inference_qyx)
+        for i, layer in enumerate(self.inference_qyx):
+            if i == num_layers - 1:
+                # last layer is gumbel softmax
+                x = layer(x, temperature, hard)
+            else:
+                x = layer(x)
+        return x
 
-    # q(z|y,x)
-    self.inference_qzyx = torch.nn.ModuleList([
-        nn.Linear(x_dim + y_dim, 512),
-        nn.ReLU(),
-        nn.Linear(512, 512),
-        nn.ReLU(),
-        Gaussian(512, z_dim)
-    ])
-
-  # q(y|x)
-  def qyx(self, x, temperature, hard):
-    num_layers = len(self.inference_qyx)
-    for i, layer in enumerate(self.inference_qyx):
-      if i == num_layers - 1:
-        #last layer is gumbel softmax
-        x = layer(x, temperature, hard)
-      else:
-        x = layer(x)
-    return x
-
-  # q(z|x,y)
-  def qzxy(self, x, y):
-    concat = torch.cat((x, y), dim=1)  
-    for layer in self.inference_qzyx:
-      concat = layer(concat)
-    return concat
-  
-  def forward(self, x, temperature=1.0, hard=0):
-    #x = Flatten(x)
-
-    # q(y|x)
-    logits, prob, y = self.qyx(x, temperature, hard)
-    
     # q(z|x,y)
-    mu, var, z = self.qzxy(x, y)
+    def qzxy(self, x, y):
+        concat = torch.cat((x, y), dim=1)
+        for layer in self.inference_qzyx:
+            concat = layer(concat)
+        return concat
 
-    output = {'mean': mu, 'var': var, 'gaussian': z, 
-              'logits': logits, 'prob_cat': prob, 'categorical': y}
-    return output
+    def forward(self, x, temperature=1.0, hard=0):
+        # x = Flatten(x)
+
+        # q(y|x)
+        logits, prob, y = self.qyx(x, temperature, hard)
+
+        # q(z|x,y)
+        mu, var, z = self.qzxy(x, y)
+
+        output = {'mean': mu, 'var': var, 'gaussian': z,
+                  'logits': logits, 'prob_cat': prob, 'categorical': y}
+        return output
 
 
 # Generative Network
-class GenerativeNet(nn.Module):
-  def __init__(self, x_dim, z_dim, y_dim):
-    super(GenerativeNet, self).__init__()
+class GenerativeNet(StorableModel):
+    def __init__(self, x_dim, z_dim, y_dim):
+        super(GenerativeNet, self).__init__()
+
+        # p(z|y)
+        self.y_mu = nn.Linear(y_dim, z_dim)
+        self.y_var = nn.Linear(y_dim, z_dim)
+
+        # p(x|z)
+        self.generative_pxz = torch.nn.ModuleList([
+            nn.Linear(z_dim, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, x_dim),
+            torch.nn.Sigmoid()
+        ])
 
     # p(z|y)
-    self.y_mu = nn.Linear(y_dim, z_dim)
-    self.y_var = nn.Linear(y_dim, z_dim)
+    def pzy(self, y):
+        y_mu = self.y_mu(y)
+        y_var = F.softplus(self.y_var(y))
+        return y_mu, y_var
 
     # p(x|z)
-    self.generative_pxz = torch.nn.ModuleList([
-        nn.Linear(z_dim, 512),
-        nn.ReLU(),
-        nn.Linear(512, 512),
-        nn.ReLU(),
-        nn.Linear(512, x_dim),
-        torch.nn.Sigmoid()
-    ])
+    def pxz(self, z):
+        for layer in self.generative_pxz:
+            z = layer(z)
+        return z
 
-  # p(z|y)
-  def pzy(self, y):
-    y_mu = self.y_mu(y)
-    y_var = F.softplus(self.y_var(y))
-    return y_mu, y_var
-  
-  # p(x|z)
-  def pxz(self, z):
-    for layer in self.generative_pxz:
-      z = layer(z)
-    return z
+    def forward(self, z, y):
+        # p(z|y)
+        y_mu, y_var = self.pzy(y)
 
-  def forward(self, z, y):
-    # p(z|y)
-    y_mu, y_var = self.pzy(y)
-    
-    # p(x|z)
-    x_rec = self.pxz(z)
+        # p(x|z)
+        x_rec = self.pxz(z)
 
-    output = {'y_mean': y_mu, 'y_var': y_var, 'x_rec': x_rec}
-    return output
+        output = {'y_mean': y_mu, 'y_var': y_var, 'x_rec': x_rec}
+        return output
 
 
 # GMVAE Network
-class GMVAENet(nn.Module):
-  def __init__(self, x_dim, z_dim, y_dim):
-    super(GMVAENet, self).__init__()
+class GMVAENet(StorableModel):
+    def __init__(self, x_dim, z_dim, y_dim):
+        super(GMVAENet, self).__init__()
 
-    self.inference = InferenceNet(x_dim, z_dim, y_dim)
-    self.generative = GenerativeNet(x_dim, z_dim, y_dim)
+        self.inference = InferenceNet(x_dim, z_dim, y_dim)
+        self.generative = GenerativeNet(x_dim, z_dim, y_dim)
 
-    # weight initialization
-    for m in self.modules():
-      if type(m) == nn.Linear or type(m) == nn.Conv2d or type(m) == nn.ConvTranspose2d:
-        torch.nn.init.xavier_normal_(m.weight)
-        if m.bias.data is not None:
-          init.constant_(m.bias, 0) 
+        # weight initialization
+        for m in self.modules():
+            if type(m) == nn.Linear or type(m) == nn.Conv2d or type(m) == nn.ConvTranspose2d:
+                torch.nn.init.xavier_normal_(m.weight)
+                if m.bias.data is not None:
+                    init.constant_(m.bias, 0)
 
-  def forward(self, x, temperature=1.0, hard=0):
-    x = x.view(x.size(0), -1)
-    out_inf = self.inference(x, temperature, hard)
-    z, y = out_inf['gaussian'], out_inf['categorical']
-    out_gen = self.generative(z, y)
-    
-    # merge output
-    output = out_inf
-    for key, value in out_gen.items():
-      output[key] = value
-    return output
+    def forward(self, x, temperature=1.0, hard=0):
+        x = x.view(x.size(0), -1)
+        out_inf = self.inference(x, temperature, hard)
+        z, y = out_inf['gaussian'], out_inf['categorical']
+        out_gen = self.generative(z, y)
+
+        # merge output
+        output = out_inf
+        for key, value in out_gen.items():
+            output[key] = value
+        return output
+
+    def save(self, path):
+        os.makedirs(path, exist_ok=True)
+
+        # gmvae_net_path = path + "/gmvae"
+        inference_net_path = path + "/inference"
+        generative_net_path = path + "/generative"
+        # self.save(gmvae_net_path)
+        self.inference.save(inference_net_path)
+        self.generative.save(generative_net_path)
+
+    def load(self, path):
+        # gmvae_net_path = path + "/gmvae"
+        inference_net_path = path + "/inference"
+        generative_net_path = path + "/generative"
+        # self.load(gmvae_net_path)
+        self.inference.load(inference_net_path)
+        self.generative.load(generative_net_path)
