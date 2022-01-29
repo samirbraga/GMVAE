808d2d7a748e8a7fd009fc7e4ba0007aec358402
samirbraga <samirchaves@gmail.com>
2022-01-29 17:10:19 -0300

    Fixing blocking loss to cuda architecture

--- pytorch/losses/LossFunctions.py
+++ pytorch/losses/LossFunctions.py
@@ -107,7 +107,7 @@ class LossFunctions:
         return -torch.mean(torch.sum(targets * log_q, dim=-1))
 
     def blocking_loss(self, logits, true_labels):
-        predicted_labels = torch.argmax(logits, dim=1).numpy()
+        predicted_labels = torch.argmax(logits, dim=1).cpu().numpy()
         allocation_matrix = build_allocation_matrix(predicted_labels, true_labels.numpy())
         acc = (allocation_matrix / allocation_matrix.sum(axis=1, keepdims=True)).max(axis=1).mean()
         return 1 - acc
